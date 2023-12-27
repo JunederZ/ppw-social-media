3c3b8750906625f20e8bbc5f5ce191b019f2e26b
JunederZ <arjunadimas000@gmail.com>
2023-12-27 16:58:14 +0700

    fix request and cookie system

--- login.js
+++ login.js
@@ -6,15 +6,23 @@ function handleLoginFormSubmit(event) {
 
     fetch('https://ppwsosmed.risalahqz.repl.co/api/login', {
         method: 'POST',
+        credentials: 'same-origin',
         headers: {
+            'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ username, password })
     })
-        .then(response => response.json())
+        .then(response => response.text())
         .then(data => {
-            if (data.success) {
+            data = JSON.parse(data);
+            console.log(data);
+            if (data.userId !== null) { 
                 console.log('Login successful');
+                window.location.replace("home.html");
+                setCookie('id', data.userId, 1);
+                setCookie('username', data.username, 1);
+                removeCookie('id');
             } else {
                 console.log('Login failed');
             }
@@ -26,3 +34,30 @@ function handleLoginFormSubmit(event) {
 
 const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', handleLoginFormSubmit);
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function removeCookie(cname) {
+    document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
